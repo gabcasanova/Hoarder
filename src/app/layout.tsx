ce84084613097aa069e1f6e70dea4e645dbff63b
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-04-07 17:14:40 -0300

    Added localization with next-intl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
 import "./globals.css"
 
-import DesktopNavbar from "../components/DesktopNavbar";
+import DesktopNavbar from "../components/navbar/DesktopNavbar";
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+import { NextIntlClientProvider } from "next-intl";
+import { getLocale } from "next-intl/server";
+
+export default async function RootLayout({children}: {children: React.ReactNode}) {
+  const locale = await getLocale()
+
   return (
-    <html>
+    <html lang={locale}>
       <body className="max-w-[1510px] min-h-screen m-auto flex flex-col bg-hoarder-dark ">
-        <header>
-          <div className="hidden xl:block">
-            <DesktopNavbar />
-          </div>
-        </header>
+        <NextIntlClientProvider>
+          <header>
+            <div className="hidden xl:block">
+              <DesktopNavbar />
+            </div>
+          </header>
 
-        <main className="p-3 flex-grow bg-hoarder-base text-white">
-          {children}
-        </main>
+          <main className="p-3 flex-grow bg-hoarder-base text-white">
+            {children}
+          </main>
 
-        <footer>
-        </footer>
+          <footer>
+          </footer>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- src/components/navbar/Searchbox.tsx
+++ src/components/navbar/Searchbox.tsx
@@ -2,17 +2,19 @@
 
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Searchbox() {
+  const t = useTranslations()
   const [query, setQuery] = useState("")
 
   return (
     <div className="flex items-center">
       <input className="hoarderTextbox w-[500px]"
         onChange={(e) => setQuery(e.target.value)} 
-        placeholder="Search tv shows, movies..." 
+        placeholder={t("navbar.searchbox")} 
         type="text"
         id="searchbox"
         value={query}

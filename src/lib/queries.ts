4dd922d6d08294a435ee94e76eeeccb009544df6
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-05-15 20:21:49 -0300

    Removed unecessary import

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,9 +1,7 @@
-import { NextResponse } from "next/server"; 
 import connectMongoDB from "./mongodb";
 import User from "../../models/user";
 
 export async function getUserByEmail(email: string) {
   await connectMongoDB();
-
   return await User.findOne({ email });
 }

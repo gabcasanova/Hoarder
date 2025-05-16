806e89fec5a9d8466e376aacffd8e59a776e4176
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-05-16 00:03:45 -0300

    Title with user's name for Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,18 @@
-export default function Page() {
+import { getTranslations } from "next-intl/server";
+import { auth } from "../../../auth";
+
+export default async function Page() {
+  const session = await auth()     // Get logged user info.
+  if (!session?.user) return null  // Show nothing if used not logged in.
+
+  const t = await getTranslations()
+  
   return (
-    <div>Dashboard page!</div>
+    <div>
+      <div className="m-5 text-center">
+        <p className="mb-2 text-2xl">{ t("dashboard.welcome") }</p>
+        <p className="text-4xl font-bold">{session.user.name}</p>
+      </div>
+    </div>
   );
 }
